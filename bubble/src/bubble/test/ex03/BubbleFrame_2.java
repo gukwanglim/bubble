d18997938ba1393df1cb8f136cb9ba07e74c69fd
gukwanglim <100823895+gukwanglim@users.noreply.github.com>
2022-11-17 04:37:48 +0900

    crash

--- bubble/src/bubble/test/ex03/BubbleFrame_2.java
+++ bubble/src/bubble/test/ex03/BubbleFrame_2.java
@@ -1,4 +1,7 @@
-package bubble.test.ex02;
+package bubble.test.ex03;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -12,6 +15,7 @@ public class BubbleFrame_2 extends JFrame {
 	public BubbleFrame_2() {
 		initObject();
 		initSetting();
+		initListener();
 		setVisible(true);
 	}
 	
@@ -33,6 +37,33 @@ public class BubbleFrame_2 extends JFrame {
 		setLayout(null);    			 	// absoulte 레이아웃 (자유롭게 그림을 그릴 수 있다.)
 		setLocationRelativeTo(null);		// JFrame 가운데 배치
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		// x버튼으로 창을 종료할 때, JVM 같이 종료
+	}
+	
+	private void initListener() {
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				System.out.println(e.getKeyCode());
+				
+				switch (e.getKeyCode()) {
+					case KeyEvent.VK_LEFT:
+						player.left();
+						break;
+						
+					case KeyEvent.VK_RIGHT:
+						player.right();
+						break;
+						
+					case KeyEvent.VK_UP:
+						player.up();
+						break;
+						
+//					case KeyEvent.VK_DOWN:
+//						player.down();
+//						break;
+				}
+			}
+		});
 	}
 	
 	public static void main(String[] args) {
--- bubble/src/bubble/test/ex05/BubbleFrame_2.java
+++ bubble/src/bubble/test/ex05/BubbleFrame_2.java
@@ -1,4 +1,7 @@
-package bubble.test.ex02;
+package bubble.test.ex05;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -12,6 +15,7 @@ public class BubbleFrame_2 extends JFrame {
 	public BubbleFrame_2() {
 		initObject();
 		initSetting();
+		initListener();
 		setVisible(true);
 	}
 	
@@ -33,6 +37,54 @@ public class BubbleFrame_2 extends JFrame {
 		setLayout(null);    			 	// absoulte 레이아웃 (자유롭게 그림을 그릴 수 있다.)
 		setLocationRelativeTo(null);		// JFrame 가운데 배치
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		// x버튼으로 창을 종료할 때, JVM 같이 종료
+	}
+	
+	private void initListener() {
+		addKeyListener(new KeyAdapter() {
+			
+			// 키보드 클릭 이벤트 핸들러
+			@Override
+			public void keyPressed(KeyEvent e) {
+				
+				switch (e.getKeyCode()) {
+					case KeyEvent.VK_LEFT:
+						if(!player.isLeft()) {
+							player.left();
+						}
+						break;
+						
+					case KeyEvent.VK_RIGHT:
+						if(!player.isRight()) {
+							player.right();
+						}
+						break;
+						
+					case KeyEvent.VK_UP:
+						if(!player.isUp() && !player.isDown()) {
+							player.up();
+						}
+						break;
+						
+//					case KeyEvent.VK_DOWN:
+//						player.down();
+//						break;
+				}
+			}
+			
+			// 키보드 해제 이벤트 핸들러
+			@Override
+			public void keyReleased(KeyEvent e) {
+				switch(e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.setLeft(false);
+					break;
+					
+				case KeyEvent.VK_RIGHT:
+					player.setRight(false);
+					break;
+				}
+			}
+		});
 	}
 	
 	public static void main(String[] args) {
